7af4dc701ebb35fe4854ed97743e3eff3b6d9dfe
Raven <druidaedoug@gmail.com>
2024-02-16 17:40:06 +0300

    V2

--- get_audio_from_youtube.py
+++ get_audio_from_youtube.py
@@ -1,19 +1,31 @@
 import yt_dlp
 import os
-import json
 
 
 def get_info(url):
-    with yt_dlp.YoutubeDL() as ydl:
-        audio_info = ydl.extract_info(url, download=False)
+    options = {
+        'simulate': True,
+        'quiet': True
+    }
 
-    audio_info_json = json.dumps(audio_info)
+    ydl = yt_dlp.YoutubeDL(options)
 
-    return audio_info_json
+    video_info = ydl.extract_info(url=url)
+
+    formats = video_info.get("formats", None)
+    a_formats = []
+
+    for audio_format in formats:
+        if audio_format['ext'] == 'm4a' or audio_format['ext'] == 'webm':
+            a_formats.append(audio_format['filesize'])
+
+    if max(a_formats) < 20971520:
+        return 'ok_size'
+
+    return 'too_big_size'
 
 
 def get_audio(url):
-    # urls = [url]
     ydl_opts = {}
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         audio_title = ydl.sanitize_info(ydl.extract_info(url, download=False))['title']
@@ -21,7 +33,7 @@ def get_audio(url):
     ydl_opts = {
         'format': 'm4a/bestaudio/best',
         'outtmpl': f'data/{audio_title}',
-        'postprocessors': [{  # Extract audio using ffmpeg
+        'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
         }],
@@ -30,12 +42,10 @@ def get_audio(url):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download(url)
 
-    audio = open(f'data/{audio_title}.mp3', 'rb')
+    if os.path.getsize(f'data/{audio_title}.mp3') <= 20971520:
+        audio = open(f'data/{audio_title}.mp3', 'rb')
+    else:
+        audio = 'This file is too big'
     os.remove(f'data/{audio_title}.mp3')
 
     return audio
-
-
-# if __name__ == '__main__':
-    # print(get_info('https://youtu.be/7HP0cOGbz_Y?si=1oU7pM-tRnsMC7NM'))
-#     get_audio(' https://youtu.be/7HP0cOGbz_Y?si=1oU7pM-tRnsMC7NM')
--- languages.py
+++ languages.py
@@ -1,17 +1,40 @@
 sentence = {
-    'english': {
+    'English': {
         'help': "To use this bot, just send a link to the video and "
-                "the bot will return you an audio file in mp3 format. \nFor all questions and in case of an error, "
+                "the bot will return you an audio file in mp3 format(Please remember that at the moment the bot only "
+                "works with files whose size does not exceed 20 MiB, sorry for the inconvenience provided)."
+                "\nFor all questions and in case of an error,"
                 "contact the e-mail druidaedoug@gmail.com address or via telegram https://t.me/koraki",
+
         'start_download': "Your audio downloading now, please wait...",
-        'download_error': "Error while downloading video, check the URL or inform us about the error"
+        'download_error': "Error while downloading video, check the URL or inform us about the error",
+        'file_size_error': 'Apologize, but this file exceeds 20 MB, I can not pass it to you :(\nFor more '
+                           'information, use the /help command',
+
+        'ok_size': '👌 Extracting audio from this video is possible',
+        'too_big_size': '🚫 Audio size in this video is too big\nUse /help for get more information',
+
+        'check_link': 'Send the link to the video from which you want to extract the audio to check the ability to '
+                      'extract:'
     },
-    'russian': {
+    'Russian': {
         'help': "Для использования этого бота достаточно отправить ссылку на видео и "
-                "бот вернет вам аудиофайл в формате mp3.\nПо всем вопросам и в случае возникновения"
+                "бот вернет вам аудиофайл в формате mp3(Пожалуйста помните, что в данный момент бот работает только с "
+                "файлами размер которых не превышает 20Мб, простите за предоставленные неудобства).\nПо всем "
+                "вопросам и в случае возникновения"
                 " ошибки обращаться по адресу электронной почты druidaedoug@gmail.com или через "
                 "телеграм https://t.me/koraki",
+
         'start_download': "Процесс загрузки начался, пожалуйста, подождите...",
-        'download_error': "Ошибка при загрузке видео, проверьте URL или сообщите нам об ошибке"
+        'download_error': "Ошибка при загрузке видео, проверьте URL или сообщите нам об ошибке",
+        'file_size_error': 'Извинете, но этот файл превышает 20 MБ, я немогу передать его вам :(\nДля получения '
+                           'дополнительной информации используйте команду /help',
+
+        'ok_size': '👌 Вы можете извлечь аудио из этого видео',
+        'too_big_size': '🚫 Размер аудио в этом видео слишком большой\nИспользуйте / help для получения '
+                        'дополнительной информации',
+
+        'check_link': 'Отправьте ссылку на видео из которой вы хотите извлечь аудио чтобы проверить возможность '
+                      'извлечения:'
     }
 }
